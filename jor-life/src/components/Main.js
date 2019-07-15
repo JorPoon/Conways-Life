c7ed5aee612f7932f94350794fad4db79776baac
Jor_Poon <jormpoon@gmail.com>
2019-07-15 17:54:43 -0400

    created new react app , deleted unneeded components, added main component and grid component

---
+++ jor-life/src/components/Main.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    state = {
+        generation: 0
+    }
+    
+    render() {
+        return (
+            <h1>Conway's Way of Life By Jor</h1>
+        )
+    }
+}
+
+export default Main
