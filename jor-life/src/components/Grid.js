7825d43f87bed7c4fefaaceb7a719019dddfb0f8
Jor_Poon <jormpoon@gmail.com>
2019-07-16 15:57:07 -0400

    creating grid box conditions for alive and dead

--- jor-life/src/components/Grid.js
+++ jor-life/src/components/Grid.js
@@ -1,1 +1,26 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Grid extends Component {
+
+    render() {
+        const width = this.props.cols * 14;
+        let rowsArr = []
+        let boxClass = "";
+        //create box id for box element
+        for(var i = 0; i < this.props.rows; i++) {
+            for (var j = 0; j < this.props.cols; j++){
+                let boxID = i + "_" + j
+
+                boxClass = this.props.fullGrid[i][j] ? "alive" : "dead"
+            }
+        }
+        return(
+            <div className='grid' style={{width: width}}>
+                {{rowsArr}}
+            </div>
+        )
+    }
+}
+
+export default Grid
--- jor-life/src/components/Main.js
+++ jor-life/src/components/Main.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Grid from './Grid'
 
 class Main extends Component {
-    state = {
-        generation: 0
+    constructor(){
+        super()
+        this.state = {
+            generation: 0,
+            fullGrid: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
+        }
+        this.speed = 100;
+        this.rows = 30
+        this.cols = 50
     }
+
     
     render() {
         return (
+            <div className= 'Main'>
             <h1>Conway's Way of Life By Jor</h1>
+            <h2>Generations: {this.state.generation}</h2>
+            <Grid 
+            fullGrid={this.state.fullGrid}
+            rows={this.rows}
+            cols={this.cols}
+            />
+            </div>
         )
     }
 }
