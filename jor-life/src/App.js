0105162c08b488df86705122dc3d5ea47bb0b3fc
Jor_Poon <jormpoon@gmail.com>
2019-07-18 16:11:40 -0400

    created routes for main component and about component

--- jor-life/src/App.js
+++ jor-life/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Main from './components/Main.js'
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './components/About.js'
+
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Main}/>
+        <Route exact path="/about" component={About}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ jor-life/src/components/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class About extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Game of Life</h1>
+            </div>
+        )
+    }
+}
+
+export default About
